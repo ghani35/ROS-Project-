1a07ba749c4e03277af3e7721348e8c3a5d0ca5a
ghani35 <bakourabdelghani1999@gmail.com>
2022-03-28 20:53:21 +0000

    docs

---
+++ assignement_3/docs/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "acilmprstuxy",
+  1: "cu",
+  2: "mru",
+  3: "acilpstxy"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions",
+  3: "Variables"
+};
+
