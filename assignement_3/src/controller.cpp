60a63a882a88b008453e9bb916ce1588a210c3f2
ghani35 <bakourabdelghani1999@gmail.com>
2022-03-28 16:43:16 +0000

    d

--- assignement_3/src/controller.cpp
+++ assignement_3/src/controller.cpp
@@ -1,6 +1,37 @@
+/**
+ * @file controller.cpp
+ * @brief This is the controller node 
+ * @author Bakour Abdelghani bakourabdelghani1999@gmail.com
+ * @version 1.0
+ * @date 25/12/2021
+ * 
+ * 
+ * Publishers to:<BR> 
+ *   /move_base/goal   /move_base/cancel  /cmd_vel  
+ * 
+ * Subscribers to:<BR>
+ * 	 /user_topic   /scan   /cmd_vel   /move_base/status
+ * 
+ * Actions :<BR>
+ *   /MoveBaseActionGoal   /GoalID   /GoalStatusArray
+ * 
+ * Description:  
+ * Control the robot depending on the **mode** entred by the user.
+ * If the user choose **mode1** he must set a goal point. The goal 
+ * can be *canceled* by pressing "4" that takes you to **mode4**
+ * and **mode3 and mode 4** are for driving the robot by the *keyboard*.
+ *
+ * 
+ * @attention This node is run by the launch file **solution.launch**
+ * @note In order for this node to **work** correctly,it is neccessary
+ * @note to have other *packages* and *nodes* running concurrently.
+ * @see user_interface.cpp
+ **/
+
+
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
-#include "assignement_3/User.h"    // header of costumized message
+#include "assignement_3/User.h"    
 #include <iostream>
 #include "geometry_msgs/Twist.h"
 #include <sstream>
@@ -8,16 +39,38 @@
 #include "actionlib_msgs/GoalID.h"
 #include "actionlib_msgs/GoalStatusArray.h"
 
-int input;  // store the mode entred by the user 
-int status; // store the status of the goal (reachable or not)
+int input;  ///< Stores the mode entred by the user 1,2,3 or 4.  
+int status; ///< Stores the status of the goal (reachable or not), returned by the *read_status* callback funtion.
+float x;///< Stores the x-axis coordinate of the goal of the form (x,y).
+float y;///< Stores the y-axis coorfinate of the goal of the form (x,y).
+float linear_x;///< Stores the linear velocity published to the robot.
+float angular_z;///< Stores the angular velocity published to the tobot.
+move_base_msgs::MoveBaseActionGoal target;///< Action used to set the goal point in case the robot is in mode1 
+actionlib_msgs::GoalID cancel; ///< Action used to cancel the goal.
 
-float x,y,linear_x,angular_z;
+ros::Publisher pub_goal; ///< Global publisher to the topic */move_base/goal*, to publish the goal point.
+ros::Publisher pub_cancel;	///< Global publisher to the topic */move_base/cancel*, to cancel the goal
+ros::Publisher pub_vel;	///< Global publisher to the topic */cmd_vel*, to publish the velocity to the robot.
 
-ros::Publisher pub_goal;
-ros::Publisher pub_cancel;	//cancel the target
-ros::Publisher pub_vel;	//publish a velocity to cmd_vel
 
-//subscriber callback to goal status array
+ros::Subscriber sub_scan; ///< Global subscriber to the topic */LaserScan*. 
+ros::Subscriber sub_vel;///< Global subscriber to the topic */cmd_vel*, to retriev the current velocity of the robot.
+ros::Subscriber sub_goal_status;///< Global subscriber to the topic */move_base/status*, to retriev the status of the goal.
+ros::Subscriber sub_user;///< Global subscriber to the topic */user_topic*, to retriev the mode entred by the user.
+
+/**
+ * @brief Callback function of the subscriber sub_goal
+ * @see sub_goal_status
+ * @param msg actionlib message stores the goal status
+ * @return nothing
+ * @attention in order to know if the goal is not reacheable 
+ * @attention the whole map should be discovered
+ * 
+ * @note This function prints the status of the goal 
+ * @note **reacheable** or **not reacheable** or **under proccesing**
+ * 
+ * 
+ **/
 void read_status(const actionlib_msgs::GoalStatusArray::ConstPtr& msg){
 
 //if_condition is used to avoid the problem when the status.list is empty (not allocated memory)
@@ -39,12 +92,34 @@ if(input ==1){
 }
 
 //subscriber callback to topic cmd_vel
+/**
+ * @brief Callback function of the subscriber sub_vel 
+ * @see sub_vel  
+ * @param msg stores the angular and linear velocity of the robot
+ * @return nothing 
+ * 
+ **/
 void read_velocity(const geometry_msgs::Twist::ConstPtr& msg){
 linear_x= msg->linear.x;
 angular_z= msg->angular.z;
 }
 
 //subscriber callback of topic(user_topic) containing the data entred by the user
+
+/**
+ * @brief Callback function of the subscriber sub_user 
+ * @see sub_user
+ * @param msg the message entred by the user 
+ * 
+ * @return nothing
+ * 
+ * @note Prints the mode choosen by the user. 
+ * @attention msg: is a customized message has the following format: 
+ * @attention int32 user
+ * @attention float32 x
+ * @attention float32 y 
+ * @warning It is called **only** when the user enters new data.
+ **/
 void user_input(const assignement_3::User::ConstPtr& msg)
 {
  //printf("status = %d \n",status);
@@ -55,7 +130,6 @@ void user_input(const assignement_3::User::ConstPtr& msg)
  if(input==1){
    printf("You are in Mode (1),Reaching a goal  \n");
    printf("you Can cancel by pressing '4' \n");
-   move_base_msgs::MoveBaseActionGoal target;
    target.goal.target_pose.pose.position.x = x;
    target.goal.target_pose.pose.position.y = y;
    target.goal.target_pose.header.frame_id = "map";
@@ -66,8 +140,6 @@ void user_input(const assignement_3::User::ConstPtr& msg)
  
  if(input==2){
    printf("You are in Mode (2), NOT ASSISTED Driving  \n");
- // publish to this topic an empty msg to cancel the goal 
-   actionlib_msgs::GoalID cancel;
    pub_cancel.publish(cancel);
  }
  if(input==3){
@@ -76,17 +148,28 @@ void user_input(const assignement_3::User::ConstPtr& msg)
  if(input==4){
    printf("canceling the target \n");
  // publish to this topic an empty msg to cancel the goal 
-   actionlib_msgs::GoalID cancel;
    pub_cancel.publish(cancel);
  }
 }
 
+
+/**
+ * @brief Callback function of the subscriber sub_scan 
+ * @see sub_scan  
+ * @param msg of type LaserScan 
+ * 
+ * @return nothing
+ * 
+ * @note Contains the code of all the 4 **modes** entred by the user 
+ * @warning It is called contineously because the robot publishes 
+ * @warning data to the /LaserScan topic very rapidely 
+ * @warning this is why the main part of the code is putted her.
+ **/
 //subscriber to scan topic
 void robotCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
 // the if statement used to cancel the target if user enters Mode2
 if(input==2){ 
-  actionlib_msgs::GoalID cancel;
   pub_cancel.publish(cancel);
  	}
 
@@ -121,7 +204,6 @@ for( i3 =0; i3 < 60; i3++){
  
         
 if(input==3){
-    actionlib_msgs::GoalID cancel;
     pub_cancel.publish(cancel);
     //check if there is a close obstacle in the front and the user drive the robot forward
     //if yes publish 0 linear velocity
@@ -152,14 +234,25 @@ if(input==3){
 }
 
 
+
+
+/**
+ * @brief the main function 
+ * @param argc
+ * @param argv
+ * @return 0
+ * 
+ * Description:
+ * initialize all the publishers and subscribers
+ **/
 int main (int argc, char **argv)
 {
 ros::init(argc, argv, "pubsub");
 ros::NodeHandle nh;
-ros::Subscriber sub_scan = nh.subscribe("/scan", 10, robotCallback);
-ros::Subscriber sub_vel = nh.subscribe("/cmd_vel", 10, read_velocity);
-ros::Subscriber sub_goal_status = nh.subscribe("/move_base/status", 100, read_status);
-ros::Subscriber sub_user = nh.subscribe("user_topic", 10, user_input);
+sub_scan = nh.subscribe("/scan", 10, robotCallback);
+sub_vel = nh.subscribe("/cmd_vel", 10, read_velocity);
+sub_goal_status = nh.subscribe("/move_base/status", 100, read_status);
+sub_user = nh.subscribe("user_topic", 10, user_input);
 pub_goal = nh.advertise<move_base_msgs::MoveBaseActionGoal> ("/move_base/goal", 100); 
 pub_cancel = nh.advertise<actionlib_msgs::GoalID> ("/move_base/cancel", 100); 
 pub_vel = nh.advertise<geometry_msgs::Twist> ("/cmd_vel", 10); 
--- assignement_3/src/user_interface.cpp
+++ assignement_3/src/user_interface.cpp
@@ -1,17 +1,62 @@
 //this node is used to publish the entred value by the use through a costumized message
 
+/**
+ * @file user_interface.cpp
+ * @brief User interface node to allow the user to **choose a mode** and **set a goal**
+ * @author Bakour Abdelghani bakourabdelghani1999@gmail.com 
+ * @version 1.0
+ * @date 25/12/2021
+ * 
+ * Description:
+ * This node asks the user to enter an **integer** value.
+ * each integer value points to a different **mode** 
+ * 
+ * 		 
+ *
+ * 
+ * 
+ * Publishers to:<BR>
+ *   /user_topic
+ * 
+ * @note These are the mode's reprentations of each integer value:
+ * @note 	1 : Set a goalpiont
+ * @note 	2 : Drive the robot by the keyboard **Not assestive**
+ * @note 	3 : Drive the robot by hte keyboard **Assistive**
+ * @note 	4 : Cancel the goal
+ *
+ * @warning in case the user chose mode **1**, he must *set a goal point*
+ *
+ * @attention The goal point is in **2D**
+ * @attention has the form (x,y), while x and y are floats
+ * @see controller.cpp 
+ *
+ **/
+
 #include "ros/ros.h"
 #include "assignement_3/User.h"
 #include <sstream>
 
-int main (int argc, char **argv)
+
+ros::Publisher pub;///<Global publisher to the topic */user_topic*, to publish the mode.
+
+/**
+ * @brief main function 
+ * @param argc 
+ * @param argv
+ * @return 0
+ * @note It prints the **mode** that has been chosen 
+ * @note and the coordinates of the goal point if it is set 
+ * 
+ **/
+
+main (int argc, char **argv)
 {
-float x,y;
-int mode;
+float x,y; 
+int mode;  
 
 ros::init(argc, argv, "User_node");
 ros::NodeHandle nh;
-ros::Publisher pub = nh.advertise<assignement_3::User> ("user_topic", 1); 
+pub = nh.advertise<assignement_3::User> ("user_topic", 1); 
 ros::Rate loop_rate(10);
 
 while (ros::ok()){
